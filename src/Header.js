02342970448a8e5c3725f808ab1844c091356f67
Semir Cohovic <semircohovic@gmail.com>
2020-08-11 21:31:23 +0200

    navbar finished

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import './Header.css';
-import SearchIcon from "@material-ui/icons/Search";
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 function Header() {
     return <nav className="header">
         <Link to="/">
@@ -11,6 +12,33 @@ function Header() {
             <input type="text" className="header__searchInput" />
             <SearchIcon className="header__searchIcon" />
         </div>
+        <div className="header__nav">
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne"> Hello Semir</span>
+                    <span className="header__optionLineTwo">Sign in</span>
+                </div>
+            </Link>
+            <Link className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne"> Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+            </Link>
+            <Link className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne"> Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+            </Link>
+            <Link to="/checkout" className="header__link">
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo">0</span>
+                </div>
+            </Link>
+        </div>
+
     </nav>
 }
 
