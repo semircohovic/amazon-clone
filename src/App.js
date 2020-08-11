e19db6175cf25753d62b95ec7f03cb6f7dbfb420
Semir Cohovic <semircohovic@gmail.com>
2020-08-11 21:48:44 +0200

    cover finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 function App() {
   return (
     <Router>
@@ -11,7 +12,7 @@ function App() {
           <Route path="/login"> <h1> Login Page</h1></Route>
           <Route path="/">
             <Header />
-            <h1> Home Page</h1>
+            <Home />
           </Route>
 
         </Switch>
